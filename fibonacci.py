991976de5b7104dfe6f2d3d1b8045a77c5e2ba0d
tomvictor <tom@buildfromzero.com>
2016-01-19 21:39:26 +0530

    adde argument passing

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,12 @@
+x = input("enter the range up to which calculate:  ")
+print ("")
 a=0
 b=1
 print(a)
 print(b)
-num =0
-while num<=10:
+x= int(x)
+num= 0
+while num<=x :
     ans = a+b
     print(ans)
     a=b
