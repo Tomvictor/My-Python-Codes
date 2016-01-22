5a72c06156e139bf7e3e5120162899b1acc22b3e
tomvictor <tom@buildfromzero.com>
2016-01-22 19:51:50 +0530

    Few more for-loop

---
+++ for.py
@@ -0,0 +1,13 @@
+for letter in 'hola':
+    print (letter)
+
+print("")
+print ('another example')
+print("")
+
+count = 0
+for letter in 'Snow!':
+    print ('Letter # ' + str(count) + ' is ' + str(letter))
+    count += 1
+    break
+print (count) 
