7bcaa56fa88734a6edf4de0f91106ef20ccd2629
tomvictor <tom@buildfromzero.com>
2016-04-28 00:11:03 +0530

    learning api

--- api.py
+++ api.py
@@ -7,19 +7,14 @@ url = 'http://developer.myntra.com/search/data/shoes'
 res = urllib.request.urlopen(url)
 
 same_url = res.geturl()
-info = res.info()
-code = res.getcode()
-
 print(same_url)
-print(code)
-
 
 str_data = res.readall().decode('utf-8')
 json_data = json.loads(str_data)
 temp = json_data['data']['results']['products']
 
 for item in temp:
-    print(item['product'])
+    print(item['price'])
 
 print(temp)
 
