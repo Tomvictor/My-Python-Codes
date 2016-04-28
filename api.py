db602137767241e43d7b5c31cf16cc722dd066b1
tomvictor <tom@buildfromzero.com>
2016-04-28 09:17:31 +0530

    image search,experience

--- api.py
+++ api.py
@@ -1,8 +1,6 @@
 import urllib.request
 import json
 
-
-
 url = 'http://developer.myntra.com/search/data/shoes'
 res = urllib.request.urlopen(url)
 
@@ -14,7 +12,6 @@ json_data = json.loads(str_data)
 temp = json_data['data']['results']['products']
 
 for item in temp:
-    print(item['price'])
+    print(item['search_image'])
 
 print(temp)
-
